1c09c2c52e87c95719dac4b54a5b38eebdde687a
Andrew Konoff <andrew.konoff@gmail.com>
2016-02-09 16:39:11 -0800

    Added listing and reservation methods.

--- airbnb/api.py
+++ airbnb/api.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import datetime
 
 API_URL = "https://api.airbnb.com"
 # API_KEY might be tied to a particular app installation, use with caution
@@ -82,6 +82,58 @@ class Api(object):
 
         return r.json()
 
+    def get_listings(self):
+        """ Gets listings for uid on api """
+        assert(self._access_token and self.uid)
+
+        r = self._session.get(API_URL + "/v1/users/" + str(self.uid) + "/listings/")
+        r.raise_for_status()
+
+        return r.json()
+
+    def get_listing(self, listing_id):
+        """ Gets listings for uid on api """
+        assert(self._access_token and self.uid)
+
+        r = self._session.get(API_URL + "/v1/listings/" + str(listing_id), params={'_format': 'v1_legacy_for_p3'})
+        r.raise_for_status()
+
+        return r.json()
+
+    def get_reservations(self, offset=0):
+        """ Gets all user's reservations (max 50) for next 120 days """
+        assert(self._access_token and self.uid)
+
+        date = datetime.date.today()
+        delta = datetime.timedelta(days=120)
+
+        future_date = date + delta
+
+        r = self._session.get(API_URL + "/v2/reservations/", 
+            params={
+            '_order': 'start_date',
+            '_limit': 50,
+            'start_date': date.strftime("%Y-%m-%d"),
+            '_offset': offset,
+            'host_id': self.uid,
+            'end_date': future_date.strftime("%Y-%m-%d"),
+            })
+        r.raise_for_status()
+
+        return r.json()
+
+    def get_reservation(self, reservation_id):
+        """ Takes reservation uid or confirmation code, returns details. """
+        assert(self._access_token and self.uid)
+
+        r = self._session.get(API_URL + "/v2/reservations/" + str(reservation_id), 
+            params={
+            '_format': 'v1_legacy_long',
+            'include_shared_itinerary': 'true',
+            })
+        r.raise_for_status()
+
+        return r.json()
 
 if __name__ == "__main__":
     import doctest
